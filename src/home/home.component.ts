df01b38bc713abcc9d65f3cb5bf1b15719ef4e53
CihanErdem <cihaneerdemdiker55@gmail.com>
2024-07-30 13:03:56 +0300

    iyi hali

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -6,18 +6,20 @@ import { LogoutComponent } from '../logout/logout/logout.component';
 import { AuthdataService } from '../auth/authdata.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BtnComponent } from "../forms/btn/btn.component";  
+import { FormsModule } from '@angular/forms';
 
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LogoutComponent, BtnComponent,CommonModule,HttpClientModule],
+  imports: [LogoutComponent, BtnComponent,CommonModule,HttpClientModule,FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{
   data: any[] = [];
+  newComment: any;
   
 
   constructor(private authService: AuthService ,private authdataService:AuthdataService,private http: HttpClient) {}
@@ -32,5 +34,34 @@ export class HomeComponent implements OnInit{
     });
   }
 
+  addComment(post: {
+      yorumlar: {
+        ppfoto: string; // Bu sizin kullanıcı fotoğrafınız olabilir
+        kisi: string; // Bu sizin kullanıcı adınız olabilir
+        yorum_tarih: string; yorum: any;
+      }[];
+    }) {
+    if (this.newComment.trim()) {
+      post.yorumlar.push({
+        ppfoto: 'assets/kisi4.jpg', // Bu sizin kullanıcı fotoğrafınız olabilir
+        kisi: 'Siz', // Bu sizin kullanıcı adınız olabilir
+        yorum_tarih: new Date().toLocaleDateString(),
+        yorum: this.newComment
+      });
+      this.newComment = '';
+    }
+  }
+
+  likePost(post: { liked: boolean; begeni: number; }) {
+    if (!post.liked) {
+      post.begeni += 1;
+      post.liked = true;
+    }
+  }
+  showLogoutMenu = false;
+
+  toggleLogoutMenu() {
+    this.showLogoutMenu = !this.showLogoutMenu;
+  }
   
 }
